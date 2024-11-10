ceee0370de5bc73e44104b0ef0b435f725a0572d
Pierre <pierre.nogaro@gmail.com>
2024-11-10 11:44:47 +0100

    maj CRUD

--- controllers/alcools.js
+++ controllers/alcools.js
@@ -1,106 +1,50 @@
-const alcools = [
-    {
-        id: "1",
-        name: "Wine",
-        degree: "12%",
-        ingredients: ["grapes", "yeast"],
-        description: "A fermented drink made from grapes, popular for its rich taste and aroma."
-    },
-    {
-        id: "2",
-        name: "Whiskey",
-        degree: "40%",
-        ingredients: ["barley", "water"],
-        description: "A distilled spirit aged in barrels, known for its complex, smoky flavor."
-    },
-    {
-        id: "3",
-        name: "Vodka",
-        degree: "40%",
-        ingredients: ["potatoes", "water"],
-        description: "A clear spirit typically made from grains or potatoes, popular in mixed drinks."
-    },
-    {
-        id: "4",
-        name: "Rum",
-        degree: "35-50%",
-        ingredients: ["sugarcane", "molasses"],
-        description: "A sweet, caramel-flavored spirit made from sugarcane or molasses, often aged in barrels."
-    },
-    {
-        id: "5",
-        name: "Tequila",
-        degree: "38-40%",
-        ingredients: ["blue agave"],
-        description: "A Mexican spirit made from the blue agave plant, known for its distinct, earthy taste."
-    }
-];
+const Alcool = require("../models/Alcool");
 
-function index(req, res) {
-    res.json(alcools);
+async function index(req, res) {
+    const alcools = await Alcool.find({});
+    res.send(alcools);
 }
 
-function show(req, res) {
-    const id = req.params.id;
-    const alcool = alcools.find(alcool => alcool.id === id);
-    if (!alcool) {
-        return res.status(404).send('Alcool not found');
-    }
-    res.json(alcool);
+async function show(req, res) {
+    const alcool = await Alcool.findById(req.params.id);
+    res.send(alcool);
 }
 
-function create(req, res) {
-    const { name, degree, ingredients, description } = req.body;
-    if (!name || !degree) {
+async function create(req, res) {
+    const { ...data } = req.body;
+
+    if (!data.name || !data.degree) {
         return res.status(400).send('Name & degree are required');
     }
 
-    const newAlcool = {
-        id: (alcools.length + 1).toString(),
-        name,
-        degree,
-        ingredients,
-        description
-    };
-
-    alcools.push(newAlcool);
+    const newAlcool = await Alcool.create({ ...data });
     res.status(201).json(newAlcool);
 }
 
-function update(req, res) {
-    const { name, degree, ingredients, description } = req.body;
-    const id = req.params.id;
-    const alcoolIndex = alcools.findIndex(alcool => alcool.id === id);
+async function update(req, res) {
+    const { ...data } = req.body;
 
-    if (alcoolIndex === -1) {
-        return res.status(404).send('Alcool not found');
-    }
-    if (!name || !degree || !ingredients || !description) {
+    if (!data.name || !data.degree) {
         return res.status(400).send('Name & degree are required');
     }
 
-    const updatedAlcool = {
-        id: alcools[alcoolIndex].id,
-        name,
-        degree,
-        ingredients,
-        description
-    };
+    const updatedAlcool = await Alcool.findByIdAndUpdate(req.params.id, data, { new: true });
 
-    alcools[alcoolIndex] = updatedAlcool;
-    res.status(200).json(updatedAlcool);
-}
-
-function deleteAlcool(req, res) {
-    const id = req.params.id;
-    const alcool = alcools.findIndex(alcool => alcool.id === id);
-
-    if (alcool === -1) {
-        return res.status(404).send('Alcool not found');
+    if (updatedAlcool) {
+        res.status(200).json(updatedAlcool);
+    } else {
+        res.status(404).send({ error: 'Alcool not found' });
     }
-
-    alcools.splice(alcool, 1);
-    res.status(200).json('Alcool deleted');
 }
 
-module.exports = { index, show, create, update, deleteAlcool };
+async function remove(req, res) {
+    const alcool = await Alcool.findByIdAndDelete(req.params.id);
+
+    if (alcool) {
+        res.status(200).json({ message: 'Alcool deleted' });
+    } else {
+        res.status(404).send({ error: 'Alcool not found' });
+    }
+}
+
+module.exports = { index, show, create, update, remove };
--- routes/alcools.js
+++ routes/alcools.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const { index, show, create, update, deleteAlcool} = require('../controllers/alcools');
+const { index, show, create, update, remove} = require('../controllers/alcools');
 const router = Router();
 
 router.get('/all', index);
 router.get('/:id', show);
 router.post('/create', create);
 router.put('/update/:id', update);
-router.delete('/delete/:id', deleteAlcool);
+router.delete('/delete/:id', remove);
 
 module.exports = router;
