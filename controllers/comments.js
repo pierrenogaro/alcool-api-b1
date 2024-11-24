5ba6fbc28645491009096cca78bda0ec1b234b49
Pierre <pierre.nogaro@gmail.com>
2024-11-24 12:01:10 +0100

    delete & update

--- controllers/comments.js
+++ controllers/comments.js
@@ -18,4 +18,42 @@ async function create(req, res) {
     res.status(201).json(comment);
 }
 
-module.exports = { create };
+async function update(req, res) {
+    const userId = req.user.userId;
+    const data = req.body;
+
+    if (!data.content) {
+        return res.status(400).send('Content is required');
+    }
+
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+        return res.status(404).send({ error: 'Comment not found' });
+    }
+
+    if (comment.author.toString() !== userId) {
+        return res.status(403).send({ error: 'You are not authorized to update this comment' });
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(req.params.id, { content: data.content }, { new: true });
+    res.status(200).json(updatedComment);
+}
+
+async function remove(req, res) {
+    const userId = req.user.userId;
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+        return res.status(404).send({ error: 'Comment not found' });
+    }
+
+    if (comment.author.toString() !== userId) {
+        return res.status(403).send({ error: 'You are not authorized to delete this comment' });
+    }
+
+    await Comment.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Comment deleted successfully' });
+}
+
+module.exports = { create, update, remove };
--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { create } = require('../controllers/comments');
+const { create, update, remove } = require('../controllers/comments');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = Router();
 
 router.post('/:alcoolId', authMiddleware, create);
+router.put('/:id', authMiddleware, update);
+router.delete('/:id', authMiddleware, remove);
 
 module.exports = router;
