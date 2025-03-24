5f733e1562350e9baf1530c8c0bc2b4a078e3e8f
Pierre <pierre.nogaro@gmail.com>
2025-03-24 18:21:50 +0100

    CORS

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connect(mongodbUri)
     });
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8081']
 }));
 
 app.use(express.json());
